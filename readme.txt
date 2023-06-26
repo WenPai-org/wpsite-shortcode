98c1f684bf0713d66fde4de19d362ed5afab738a
feng <f2010525@gmail.com>
2023-06-26 12:33:52 +0800

    更新至 1.1
     修复 bug

--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Tags: Shortcode,Dynamic Data,Metadata,SEO
 Requires at least: 5.4
 Tested up to: 6.2.2
 Requires PHP: 5.6
-Stable tag: 1.1
+Stable tag: 1.1.1
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- wpsite-shortcode.php
+++ wpsite-shortcode.php
@@ -7,7 +7,7 @@
  * Author URI: https://wpsite.cn/
  * Text Domain: wpsite-shortcode
  * Domain Path: /languages
- * Version: 1.1
+ * Version: 1.1.1
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  *
